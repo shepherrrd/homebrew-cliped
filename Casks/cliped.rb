7c6e2d396c6f100ded0b4d490328701c8f0ec88d
shepherrrd <shefkid2@gmail.com>
2025-07-07 10:03:40 +0100

    HomeBrew Update

--- Casks/cliped.rb
+++ Casks/cliped.rb
@@ -1,6 +1,6 @@
 cask "cliped" do
   version "1.0.0"
-  sha256 "9dc74d4ce140e5cdc753b52bdb936579d16ec11fedf30172781aa0566f954386"
+  sha256 "235fc9fdbf0c7f25c27b702f7b2f8f6ef1f069ce4e0a50701564c479e73f41e1"
 
   url "https://github.com/shepherrrd/homebrew-cliped/releases/download/v1.0.0/Cliped.dmg"
   name "Cliped"
