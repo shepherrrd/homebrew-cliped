ae8ef2b453dbc65303541922ca8121b08af11823
shepherrrd <shefkid2@gmail.com>
2025-07-07 09:32:35 +0100

    Working Update

--- Casks/clipboard-manager.rb
+++ Casks/clipboard-manager.rb
@@ -1,12 +1,12 @@
 cask "clipboard-manager" do
   version "1.0.0"
-  sha256 "fcce75d88f518e5efdbf4a9aa7d40c475bfea2e260315eaa29bdc11ad749c11a"
+  sha256 "9dc74d4ce140e5cdc753b52bdb936579d16ec11fedf30172781aa0566f954386"
 
-  url "https://github.com/shepherrrd/homebrew-clipboard-manager/releases/download/v1.0.0/ClipboardManager.dmg"
-  name "Clipboard Manager"
+  url "https://github.com/shepherrrd/homebrew-cliped/releases/download/v1.0.0/Cliped.dmg"
+  name "Cliped"
   desc "Simple clipboard history manager for macOS"
-  homepage "https://github.com/shepherrrd/homebrew-clipboard-manageri"
+  homepage "https://github.com/shepherrrd/homebrew-cliped"
 
-  app "ClipBoardManager.app"
+  app "Cliped.app"
 end
 
